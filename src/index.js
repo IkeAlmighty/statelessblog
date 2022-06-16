1f798c37622f1afc457a1482cd587ddebe5fba07
ikealmighty <isaacyates7@gmail.com>
2022-06-16 18:08:15 -0500

    slight fix for heroku dynamic ports

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`starting server on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`starting server on port ${process.env.PORT || 3000}`)
 );
